aacfb5ecccaa60fd079f4adf46dca19cbdd836e9
Midhun <midhunskani@gmail.com>
2024-07-20 09:42:52 +0530

    4 - findMedianSortedArrays

---
+++ 4_findMedianSortedArrays.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        num3 = nums1 + nums2
+        num3.sort()
+        len_num3 = len(num3)
+        if len_num3%2 == 0:
+            a = len_num3//2
+            b = a - 1
+            return (num3[a]+num3[b])/2
+        else:
+            a = len_num3//2
+            return num3[a]
+            
+ss = Solution()
+nums1 = []
+nums2 = [1]
+print(ss.findMedianSortedArrays(nums1, nums2))
