d0dce626e7fe7538ca02921ea9fd4fae434948bd
Midhun <midhunskani@gmail.com>
2024-07-21 14:55:24 +0530

    414-thirdMax

---
+++ 414_thirdMax.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = list(set(nums))
+        nums.sort(reverse=True)
+        nums = nums[0:3]
+        if len(nums)==3:
+            return nums[2]
+        return nums[0]
+
+ss = Solution()
+nums = [3,2,1]
+print(ss.thirdMax(nums))
