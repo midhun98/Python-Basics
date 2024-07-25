d5b1ae554d333525cdc3104684c7a34837077932
Midhun <midhunskani@gmail.com>
2024-07-25 10:55:22 +0530

    387_firstUniqChar.py

---
+++ 387_firstUniqChar.py
@@ -0,0 +1,17 @@
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        dic = {}
+        for letter in s:
+            if letter not in dic:
+                dic[letter] = 1
+            else:
+                dic[letter] += 1
+
+        for index, value in enumerate(s):
+            if dic[value] == 1:
+                return index       
+        return -1
+
+ss = Solution()
+s = "loveleetcode"
+print(ss.firstUniqChar(s))
