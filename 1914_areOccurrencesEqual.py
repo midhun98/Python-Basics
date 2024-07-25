7cf47d74405d1ebbe4347d1dd7729f0ce409c3f6
Midhun <midhunskani@gmail.com>
2024-07-25 19:56:20 +0530

    Minor changes

--- 1914_areOccurrencesEqual.py
+++ 1914_areOccurrencesEqual.py
@@ -2,11 +2,14 @@ from collections import Counter
 
 class Solution:
     def areOccurrencesEqual(self, s: str) -> bool:
-        count_dict = dict(Counter(s))
+        count_dict = Counter(s)
         if len(set(count_dict.values())) == 1:
             return True
         return False
 
+        # count_dict = Counter(s)
+        # return len(set(count_dict.values())) == 1
+        
 ss = Solution()
 s = "abacbc"
 print(ss.areOccurrencesEqual(s))
