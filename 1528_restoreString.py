4d0368f7aa39b8789e115850293a0711d03ee74e
Midhun <midhunskani@gmail.com>
2024-07-26 18:20:10 +0530

    1528_restoreString.py

---
+++ 1528_restoreString.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        map = dict(zip(indices,list(s)))
+        li = []
+        for number in range(len(s)):
+            li.append(map[number])
+        return ''.join(li)
+
+ss = Solution()
+s = "codeleet"
+indices = [4,5,6,7,0,1,2,3]
+print(ss.restoreString(s, indices))
