8e7c01ad21864a8d994cfcc1cce54ed306e9f3f3
Midhun <midhunskani@gmail.com>
2024-07-28 11:07:27 +0530

    bubble Sort

---
+++ bubbleSort.py
@@ -0,0 +1,9 @@
+def bubble_sort(nums):
+    for i in range(len(nums)-1,0,-1):
+        for j in range(i):
+            if nums[j] > nums[j+1]:
+                nums[j],nums[j+1] = nums[j+1], nums[j]
+    return nums
+
+nums = [5,4,3,2,1]
+print(bubble_sort(nums))
