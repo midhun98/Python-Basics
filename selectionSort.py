c5b085be154cb65ba9c37177235d56f4205fea31
Midhun <midhunskani@gmail.com>
2024-07-28 11:35:12 +0530

    selection sort algorithm

---
+++ selectionSort.py
@@ -0,0 +1,11 @@
+def selection_sort(nums):
+    for i in range(len(nums)):
+        minpos = i
+        for j in range(i, len(nums)):
+            if nums[j] < nums[minpos]:
+                minpos = j
+        nums[i], nums[minpos] =  nums[minpos], nums[i] 
+    return nums
+
+nums = [5,4,3,2,1]
+print(selection_sort(nums))
