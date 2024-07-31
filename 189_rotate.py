1837f2ed779eec8a2b2d8801979b761dca6cadc9
Midhun <midhunskani@gmail.com>
2024-07-31 10:55:44 +0530

    80, 189 LeetCode

---
+++ 189_rotate.py
@@ -0,0 +1,15 @@
+from typing import List
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for _ in range(k):
+            num = nums.pop()
+            nums.insert(0, num)
+
+ss = Solution()
+nums = [1,2,3,4,5,6,7]
+k = 3
+print(ss.rotate(nums,k))
---
+++ 80_removeDuplicates.py
@@ -0,0 +1,15 @@
+from collections import Counter
+from typing import List
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        count = Counter(nums)
+        result = []  
+        for number in count:
+            result.extend([number] * min(count[number], 2))
+        nums[:] = result
+        return len(result)  
+
+ss = Solution()
+nums = [1,1,1,2,2,3]
+print(ss.removeDuplicates(nums))
