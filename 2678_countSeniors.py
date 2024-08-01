b7699af892c324fe8060ad49a5816638c3d5142c
Midhun <midhunskani@gmail.com>
2024-08-01 08:53:12 +0530

    2678 countSeniors

---
+++ 2678_countSeniors.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def countSeniors(self, details: List[str]) -> int:
+        count = 0
+        for data in details:
+            age = int(data[11:13])
+            if age > 60:
+                count += 1
+        return count
+
+ss = Solution()
+details = ["7868190130M7522","5303914400F9211","9273338290F4010"]
+print(ss.countSeniors(details))
