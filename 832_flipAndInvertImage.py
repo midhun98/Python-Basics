0fdc289de1ecb0f0b6f7cf0835dfaedd67f6b876
Midhun <midhunskani@gmail.com>
2024-08-06 10:31:00 +0530

    824, 832

---
+++ 832_flipAndInvertImage.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def flipAndInvertImage(self, image: List[List[int]]) -> List[List[int]]:
+        out = []
+        for li in image:
+            li.reverse()
+            out.append([val ^ 1 for val in li])
+        return out
+
+ss = Solution()
+image = [[1,1,0],[1,0,1],[0,0,0]]
+print(ss.flipAndInvertImage(image))
