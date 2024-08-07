9406df3ccf92f763c07177f9da01fc7a77a44857
Midhun <midhunskani@gmail.com>
2024-08-07 18:18:22 +0530

    __name__ == "__main__"

---
+++ __name__ basics/demo.py
@@ -0,0 +1,17 @@
+from Calc import add
+
+print("__name__ is ", __name__)
+
+def fun1():
+    add()
+    print("from fun1")
+
+def fun2():
+    print("from fun2")
+
+def main():
+    fun1()
+    fun2()
+
+
+main()
