820e4e01af9ad459196ab71bdce38f59526b63ac
Midhun <midhunskani@gmail.com>
2024-08-13 22:10:03 +0530

    287, 448

---
+++ 287_findDuplicate.py
@@ -0,0 +1,14 @@
+from typing import List
+from collections import Counter
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        map = Counter(nums)
+        for key, value in map.items():
+            if value > 1:
+                return key
+
+
+ss = Solution()
+nums = [1,3,4,2,2]
+print(ss.findDuplicate(nums))
