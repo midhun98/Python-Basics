f030e52f51440a50e318f9b5d388b7b5fac77634
Midhun <midhunskani@gmail.com>
2024-08-17 17:12:04 +0530

    3210 getEncryptedString

---
+++ 3210_getEncryptedString.py
@@ -0,0 +1,15 @@
+class Solution:
+    def getEncryptedString(self, s: str, k: int) -> str:
+        out = ''
+        counter = 0
+        n = len(s)
+        for _ in s:
+            num = (counter + k) % n
+            out += s[num]
+            counter += 1
+        return out
+
+ss = Solution()
+s = "dart"
+k = 3
+print(ss.getEncryptedString(s, k))
