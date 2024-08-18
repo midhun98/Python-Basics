915e9d2de87e330a9d8a8b75d0b3b223f3d56771
Midhun <midhunskani@gmail.com>
2024-08-18 15:26:46 +0530

    73 setZeroes

---
+++ 73_setZeroes.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        out = []
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    out.append([i,j])
+        
+        cols = []
+        rows = []
+        for li in out:
+            rows.append(li[0])
+            cols.append(li[1])
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i in rows or j in cols:
+                    matrix[i][j] = 0
+
+ss = Solution()
+matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+print(ss.setZeroes(matrix))
