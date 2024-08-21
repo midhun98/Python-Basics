86c0e530afb4ab4fe20e115f911bf9e7390a113a
Midhun <midhunskani@gmail.com>
2024-08-21 18:17:14 +0530

    2011, 2942

---
+++ 2942_findWordsContaining.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        out = []
+        for index, word in enumerate(words):
+            if x in word:
+                out.append(index)
+        return out
+
+ss = Solution()
+words = ["abc","bcd","aaaa","cbc"]
+print(ss.findWordsContaining(words))        
