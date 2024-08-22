06fc01f20359d59028e29b3120ba231053dddc4d
Midhun <midhunskani@gmail.com>
2024-08-22 19:42:42 +0530

    3110 scoreOfString

---
+++ 3110_scoreOfString.py
@@ -0,0 +1,11 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        ascii_values = [ord(c) for c in s]
+        out = []
+        for i in range(len(ascii_values) - 1):
+            out.append(abs(ascii_values[i] - ascii_values[i + 1]))
+        return sum(out)
+
+ss = Solution()
+s = "hello"
+print(ss.scoreOfString(s))
