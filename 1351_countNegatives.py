3b6481e2719402c36bb4b6b1d236c2ac21e70b40
Midhun <midhunskani@gmail.com>
2024-08-22 20:46:26 +0530

    704, 1351

---
+++ 1351_countNegatives.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        out = 0
+        for li in grid:
+            out += sum(1 for i in li if i < 0)
+        return out
+
+ss = Solution()
+grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+print(ss.countNegatives(grid))
---
+++ 704_search.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+        while low <= high:
+            mid = (low + high) // 2
+            mid_number = nums[mid]
+            if mid_number == target:
+                return mid
+            elif mid_number < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
+
+
+ss = Solution()
+nums = [-1,0,3,5,9,12]
+target = 3
+print(ss.search(nums, target))
