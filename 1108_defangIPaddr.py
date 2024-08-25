c308442c5db91f4765c19bfea3ff0e533f214227
Midhun <midhunskani@gmail.com>
2024-08-25 11:00:20 +0530

    1108 defangIPaddr

---
+++ 1108_defangIPaddr.py
@@ -0,0 +1,19 @@
+class Solution1:
+    def defangIPaddr(self, address: str) -> str:
+        out = ""
+        for char in address:
+            if char == ".":
+                out += "[.]"
+            else:
+                out += char
+        return out
+
+
+class Solution2:
+    def defangIPaddr(self, address: str) -> str:
+        return address.replace(".", "[.]")
+
+
+ss = Solution1()
+address = "1.1.1.1"
+print(ss.defangIPaddr(address))
