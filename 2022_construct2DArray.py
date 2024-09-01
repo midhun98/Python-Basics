2617c9fabbdd167fe30de6d5a2e95af15ef0cb52
Midhun <midhunskani@gmail.com>
2024-09-01 11:29:19 +0530

    2022, 2520, 2744

---
+++ 2022_construct2DArray.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def construct2DArray(self, original: List[int], m: int, n: int) -> List[List[int]]:
+        if m * n != len(original):
+            return []
+
+        out = []
+        pointer = 0
+        for _ in range(m):
+            li = []
+            for _ in range(n):
+                li.append(original[pointer])
+                pointer += 1
+            out.append(li)
+        return out
+
+
+ss = Solution()
+original = [1, 2, 3]
+m = 1
+n = 3
+print(ss.construct2DArray(original, m, n))
---
+++ 2520_countDigits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countDigits(self, num: int) -> int:
+        numbers = [int(i) for i in str(num)]
+        count = 0
+        for n in numbers:
+            if num%n == 0:
+                count += 1
+        return count
+
+ss = Solution()
+num = 1248
+print(ss.countDigits(num))
