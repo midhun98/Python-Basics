1cedd6374c1210bd035dea18a72c41e613ae7cae
Midhun <midhunskani@gmail.com>
2024-09-02 21:41:15 +0530

    1894 chalkReplacer

---
+++ 1894_chalkReplacer.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def chalkReplacer(self, chalk: List[int], k: int) -> int:
+        total_chalk = sum(chalk)
+        remaining_chalk = k % total_chalk
+        for index, number in enumerate(chalk):
+            remaining_chalk -= number
+            if remaining_chalk < 0:
+                return index
+
+
+ss = Solution()
+chalk = [3, 4, 1, 2]
+k = 25
+print(ss.chalkReplacer(chalk, k))
