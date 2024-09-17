57346c2b210adec970e6b4bded3a4fe348fcb1bd
Midhun <midhunskani@gmail.com>
2024-09-17 09:47:04 +0530

    884 uncommonFromSentences

---
+++ 884_uncommonFromSentences.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        data = {}
+        s3  = s1 + " " +  s2
+        s3 = s3.split()
+        for word in s3:
+            if word not in data:
+                data[word] = 1
+            else:
+                data[word] += 1
+        out = []
+        for key, value in data.items():
+            if value == 1:
+                out.append(key)
+        return out
+
+ss = Solution()
+s1 = "this apple is sweet"
+s2 = "this apple is sour"
+print(ss.uncommonFromSentences(s1, s2))
