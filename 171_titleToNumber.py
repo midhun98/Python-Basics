ddc7bc0d8a8c68fc7abdeff0d784ac13c0ed7a42
Midhun <midhunskani@gmail.com>
2024-09-27 22:05:34 +0530

    171 titleToNumber

---
+++ 171_titleToNumber.py
@@ -0,0 +1,13 @@
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        out = 0
+        columnTitle = columnTitle[::-1]
+        for index, letter in enumerate(columnTitle):
+            num = ord(letter) - 64
+            out += 26**index * num
+        return out
+
+
+ss = Solution()
+columnTitle = "BB"
+print(ss.titleToNumber(columnTitle))
