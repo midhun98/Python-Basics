d9d79067c7ff02216e3e9b3da5f2182f0b67ffec
Midhun <midhunskani@gmail.com>
2024-09-29 12:29:48 +0530

    153 findMin

--- 153_findMin.py
+++ 153_findMin.py
@@ -3,16 +3,14 @@ from typing import List
 
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        low, high = 0, len(nums) - 1
-        while low <= high:
-            if nums[low] <= nums[high]:
-                return nums[low]
-            mid = (low + high) // 2
-            if nums[low] > nums[mid]:
-                high = mid
-            if nums[low] < nums[mid]:
-                low = mid + 1
-
+        left, right = 0, len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > nums[right]:
+                left = mid + 1
+            if nums[mid] < nums[right]:
+                right = mid
+        return nums[left]
 
 ss = Solution()
 nums = [4, 5, 6, 7, 0, 1, 2]
