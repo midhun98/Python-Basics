616e7724f3ea4b7a21591e2bd856048ffd7f166a
Midhun <midhunskani@gmail.com>
2024-09-30 20:17:08 +0530

    48 rotate

---
+++ 48_rotate.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):  # for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for row in matrix:
+            row.reverse()
+
+
+ss = Solution()
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(ss.rotate(matrix))
