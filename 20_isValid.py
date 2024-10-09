301a3dbc8c81ff63e669726c607f7d2e0d1e1db4
Midhun <midhunskani@gmail.com>
2024-10-09 09:52:35 +0530

    20, 921

--- 20_isValid.py
+++ 20_isValid.py
@@ -15,6 +15,21 @@ class Solution:
         return True if not stack else False
 
 
-ss = Solution()
-s = "()[]{}"
+class Solution2:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        for char in s:
+            if char == "(":
+                stack.append(")")
+            elif char == "[":
+                stack.append("]")
+            elif char == "{":
+                stack.append("}")
+            elif not stack or stack.pop() != char:
+                return False
+        return not stack
+
+
+ss = Solution2()
+s = "()[]{}}"
 print(ss.isValid(s))
---
+++ 921_minAddToMakeValid.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        open_count = 0
+        close_count = 0
+        for char in s:
+            if char == "(":
+                open_count += 1
+            else:
+                open_count -= 1
+                if open_count < 0:
+                    open_count = 0
+                    close_count += 1
+        return open_count + close_count
+
+ss = Solution()
+s = ")))"
+print(ss.minAddToMakeValid(s))
