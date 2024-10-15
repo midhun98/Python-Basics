06e038ca93c6de1037c0249bdee9a3912ed52f79
Midhun <midhunskani@gmail.com>
2024-10-15 17:15:31 +0530

    155, 2938

---
+++ 2938_minimumSteps.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumSteps(self, s: str) -> int:
+        steps = 0
+        one_count = 0
+        for char in s:
+            if char == "1":
+                one_count += 1
+            if char == "0":
+                steps += one_count
+        return steps
+
+ss = Solution()
+s = "0110"
+print(ss.minimumSteps(s))
