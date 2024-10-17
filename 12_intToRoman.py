4c6620006dc9c566fc71695adfcbf4e954621968
Midhun <midhunskani@gmail.com>
2024-10-17 21:22:34 +0530

    12_intToRoman

---
+++ 12_intToRoman.py
@@ -0,0 +1,31 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        data = {
+            "I": 1,
+            "IV": 4,
+            "V": 5,
+            "IX": 9,
+            "X": 10,
+            "XL": 40,
+            "L": 50,
+            "XC": 90,
+            "C": 100,
+            "CD": 400,
+            "D": 500,
+            "CM": 900,
+            "M": 1000,
+        }
+        out = []
+        for index, value in reversed(data.items()):
+            while num > 0:
+                if num >= value:
+                    out.append(index)
+                    num -= value
+                else:
+                    break
+        return "".join(out)
+
+
+ss = Solution()
+num = 1994
+print(ss.intToRoman(num))
