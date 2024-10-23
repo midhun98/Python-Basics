d69962c7393255c2c6765b7dca219bb5e2cc8c9b
midhun98 <midhunskani@gmail.com>
2024-10-23 22:16:04 +0530

    2239 findClosestNumber

---
+++ 2239_findClosestNumber.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+
+ss = Solution()
+nums = [2,-1,1]
+
+print(ss.findClosestNumber(nums))class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        out = nums[0]
+        for num in nums:
+            if abs(num) < abs(out):
+                out = num
+                out = max(num, out)
+        if out < 0 and abs(out) in nums :
+            return abs(out)
+        else:
+            return out
