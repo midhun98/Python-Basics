34637a14c98b7ba133b033bd3cd3c34c316c6574
midhun98 <midhunskani@gmail.com>
2024-10-28 22:20:26 +0530

    2864_maximumOddBinaryNumber

---
+++ 2864_maximumOddBinaryNumber.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        one_count = 0
+        zero_count = 0
+
+        for num in s:
+            if int(num) == 0:
+                zero_count += 1
+            else:
+                one_count += 1
+        out = "1" * one_count + "0" * zero_count
+        out = out[1:]
+        return out + "1"
+
+
+ss = Solution()
+s = "0101"
+print(ss.maximumOddBinaryNumber(s))
