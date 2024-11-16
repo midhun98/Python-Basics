7f8ac823c2188c0b690f123a8af188757090c6bd
midhun98 <midhunskani@gmail.com>
2024-11-16 18:38:31 +0530

    3 .lengthOfLongestSubstring

---
+++ 3_lengthOfLongestSubstring.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if s == "":
+            return 0
+        li = []
+        length = len(s)
+        for left in range(length):
+            sub = ""
+            for r in range(left, length):
+                if s[r] in sub:
+                    break
+                sub += s[r]
+            li.append(sub)
+        li = [len(i) for i in li]
+        return max(li)
+
+
+ss = Solution()
+s = "pwwkew"
+print(ss.lengthOfLongestSubstring(s))
