f05ba6bdb74686ed1a8d70737d1f54380ce7cd39
midhun98 <midhunskani@gmail.com>
2025-01-16 23:02:03 +0530

    3223_minimumLength.py

---
+++ 3223_minimumLength.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        count = Counter(s)
+        out = 0
+        for num in count.values():
+            if num % 2 == 0:
+                out += 2
+            else:
+                out += 1
+        return out
+
+
+ss = Solution()
+s = "abaacbcbb"
+print(ss.minimumLength(s))
