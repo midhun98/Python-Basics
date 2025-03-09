ef01b70ec97769d32196026642731fb217b2612d
midhun98 <midhunskani@gmail.com>
2025-03-09 17:28:43 +0530

    2379

---
+++ 2379_minimumRecolors.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        min_white = blocks[:k].count("W")
+        curr_white = min_white
+
+        for i in range(k, len(blocks)):
+            if blocks[i - k] == "W":
+                curr_white -= 1
+            if blocks[i] == "W":
+                curr_white += 1
+            min_white = min(min_white, curr_white)
+        return min_white
+
+
+ss = Solution()
+blocks = "WBBWWBBWBW"
+k = 7
+print(ss.minimumRecolors(blocks, k))
