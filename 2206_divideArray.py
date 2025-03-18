92601f94101b8aa01743f1330dd5ab680d61a3e2
midhun98 <midhunskani@gmail.com>
2025-03-18 22:53:17 +0530

    2206

---
+++ 2206_divideArray.py
@@ -0,0 +1,17 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        data = dict(Counter(nums))
+        li = data.values()
+        for num in li:
+            if num % 2 != 0:
+                return False
+        return True
+
+
+ss = Solution()
+nums = [3, 2, 3, 2, 2, 2]
+print(ss.divideArray(nums))
