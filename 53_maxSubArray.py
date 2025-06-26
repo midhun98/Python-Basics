55907eb1c23a78488d989101cd5789fa77ca910e
Midhun <midhunskani@gmail.com>
2025-06-26 21:00:08 +0530

    learn 53 max profit

--- 53_maxSubArray.py
+++ 53_maxSubArray.py
@@ -2,14 +2,15 @@ from typing import List
 
 
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        max_sum = nums[0]
-        current_sum = 0
-        for num in nums:
-            current_sum += num
-            max_sum = max(current_sum, max_sum)
-            current_sum = max(current_sum, 0)
-        return max_sum
+	def maxSubArray(self, nums: List[int]) -> int:
+		max_sum = nums[0]
+		curr_max = nums[0]
+
+		for i in range(1, len(nums)):
+			curr_max = max(nums[i], curr_max+nums[i])
+			max_sum = max(curr_max, max_sum)
+
+		return max_sum
 
 
 ss = Solution()
