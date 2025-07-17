319a0b4a625726345edd4ecbacdb04d69d94eaef
Midhun <midhunskani@gmail.com>
2025-07-17 23:26:51 +0530

    gfg practice

---
+++ 75_sortColors.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+	def sortColors(self, nums: List[int]) -> None:
+		"""
+		Do not return anything, modify nums in-place instead.
+		"""
+		low = 0
+		mid = 0
+		high = len(nums) - 1
+
+		while mid <= high:
+			if nums[mid] == 0:
+				# nums[low], nums[mid] = nums[mid], nums[low]
+				low += 1
+				mid += 1
+			elif nums[mid] == 1:
+				mid += 1
+			else:  # nums[mid] == 2
+				nums[mid], nums[high] = nums[high], nums[mid]
+				high -= 1
+
+		return nums
+
+ss = Solution()
+nums = [2,0,2,1,1,0]
+print(ss.sortColors(nums))
