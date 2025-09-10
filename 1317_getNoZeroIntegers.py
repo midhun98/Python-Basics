0bcb8742b7f846a88aadff1e65d72c8b13a84280
Midhun <midhunskani@gmail.com>
2025-09-10 21:41:45 +0530

    1317

---
+++ 1317_getNoZeroIntegers.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+	def getNoZeroIntegers(self, n: int) -> List[int]:
+		for A in range(1, n):
+			B = n - A
+			if '0' not in str(A) and '0' not in str(B):
+				return [A, B]
+
+ss = Solution()
+n = 2
+print(ss.getNoZeroIntegers(n))
