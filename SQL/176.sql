93e880c7b231fc369a7e555737eb5373afe66808
Midhun <midhunskani@gmail.com>
2025-10-15 21:19:36 +0530

    sql 176

--- SQL/176.sql
+++ SQL/176.sql
@@ -4,3 +4,14 @@ SELECT (
         ORDER BY salary DESC OFFSET 1
         LIMIT 1
     ) AS SecondHighestSalary;
+-- 
+-- using dense rank
+SELECT MAX(salary) AS SecondHighestSalary
+FROM (
+        SELECT salary,
+            DENSE_RANK() OVER (
+                ORDER BY salary DESC
+            ) AS rnk
+        FROM Employee
+    ) t
+WHERE rnk = 2;
