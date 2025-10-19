36952b525f269dfbe12c63d721f23c3118d519d4
Midhun <midhunskani@gmail.com>
2025-10-19 18:57:19 +0530

    repeatedSubstringPattern

--- 459_repeatedSubstringPattern.py
+++ 459_repeatedSubstringPattern.py
@@ -7,6 +7,18 @@ class Solution:
 		return s in data
 
 
+
+class Solution2:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        rep = ''
+
+        for i in range(len(s)//2):
+            rep += s[i]
+            if (len(s) % len(rep)) == 0:
+                  if rep * (len(s) // len(rep)) == s:
+                        return True
+        return False
+
 ss = Solution()
 s = "abcabcabcabc"
 print(ss.repeatedSubstringPattern(s))
