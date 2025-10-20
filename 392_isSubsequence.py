f7f3a7b9750b7782fef4d611421a3205be4ef33b
Midhun <midhunskani@gmail.com>
2025-10-20 22:57:51 +0530

    392_isSubsequence.py

---
+++ 392_isSubsequence.py
@@ -0,0 +1,15 @@
+class Solution:
+	def isSubsequence(self, s: str, t: str) -> bool:
+		sp = 0
+		tp = 0
+
+		while sp < len(s) and tp < len(t):
+			if s[sp] == t[tp]:
+				sp += 1
+			tp += 1
+		return sp == len(s)
+        
+ss = Solution()
+s = "abc"
+t = "ahbgdc"
+print(ss.isSubsequence(s, t))
