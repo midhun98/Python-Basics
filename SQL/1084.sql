f352d139227985ed5712383e9657657e62c93d58
Midhun <midhunskani@gmail.com>
2025-11-10 23:08:01 +0530

    Create 1084.sql

---
+++ SQL/1084.sql
@@ -0,0 +1,10 @@
+WITH CTE AS
+(SELECT product_id
+FROM Sales
+WHERE sale_date  > '2019-03-31' OR sale_date < '2019-01-01')
+
+SELECT distinct s.product_id, p.product_name
+FROM Sales s
+LEFT JOIN Product p
+ON s.product_id = p.product_id
+WHERE s.product_id NOT IN(SELECT product_id FROM CTE)
