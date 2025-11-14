0af32ffc63adfd792539a19d340eda5957acef1f
Midhun <midhunskani@gmail.com>
2025-11-14 22:37:00 +0530

    Changes

--- 3228_maxOperations.py
+++ 3228_maxOperations.py
@@ -1,18 +1,18 @@
+# https://leetcode.com/problems/maximum-number-of-operations-to-move-ones-to-the-end/solutions/7344834/solution-by-la_castille-vdqt
 class Solution:
     def maxOperations(self, s: str) -> int:
-        s = [int(i) for i in list(s)]
+        arr = [int(i) for i in s]
         pref = []
         ones_count = 0
-        for num in s:
+        for num in arr:
             if num == 1:
                 ones_count += 1
                 pref.append(ones_count)
             else:
                 pref.append(ones_count)
-
         li = []
         for i in range(len(s)):
-            if s[i] == 0:
+            if arr[i] == 0:
                 li.append(pref[i])
 
         return sum(set(li))
