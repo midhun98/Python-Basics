ed13ed21eb3504f00a7b17ccd3972e0cc981b16c
Midhun <midhunskani@gmail.com>
2025-11-16 10:13:33 +0530

    Update 1513_numSub.py

--- 1513_numSub.py
+++ 1513_numSub.py
@@ -1,15 +1,15 @@
 class Solution:
-    def numSub(self, s: str) -> int:
-        res = 0
-        cons = 0
+	def numSub(self, s: str) -> int:
+		res = 0
+		cons = 0
 
-        for num in s:
-            if num == '1':
-                cons += 1
-                res += cons
-            else:
-                cons = 0
-        return res
+		for num in s:
+			if num == "1":
+				cons += 1
+				res += cons
+			else:
+				cons = 0
+		return res % (10**9 + 7)
 
 
 ss = Solution()
