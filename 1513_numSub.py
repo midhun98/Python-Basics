abaa6e12c3d84063917a29afedb8abcd207735af
Midhun <midhunskani@gmail.com>
2025-11-16 10:08:57 +0530

    Create 1513_numSub.py

---
+++ 1513_numSub.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numSub(self, s: str) -> int:
+        res = 0
+        cons = 0
+
+        for num in s:
+            if num == '1':
+                cons += 1
+                res += cons
+            else:
+                cons = 0
+        return res
+
+
+ss = Solution()
+s = "0110111"
+print(ss.numSub(s))
