2a3e82d5370fdb8317243cb9aaac1f578310d5cf
Midhun <midhunskani@gmail.com>
2025-11-22 22:18:32 +0530

    3498reverseDegree

---
+++ 3498_reverseDegree.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        res = 0
+        data = {'a':26,'b':25,'c':24,'d':23,'e':22,'f':21,'g':20,'h':19,'i':18,'j':17,'k':16,'l':15,'m':14,'n':13,'o':12,'p':11,'q':10,'r':9,'s':8,'t':7,'u':6,'v':5,'w':4,'x':3,'y':2,'z':1}
+        new_s = list(s)
+        for index, char in enumerate(new_s):
+            res += data[char] * (index + 1)
+        return res
+
+ss = Solution()
+s = "abc"
+print(ss.reverseDegree(s))
