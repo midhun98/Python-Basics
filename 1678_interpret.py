df393faea3992f64feec27502c1ccb0fddcae63b
Midhun <midhunskani@gmail.com>
2025-11-24 22:34:10 +0530

    1678 interpret

---
+++ 1678_interpret.py
@@ -0,0 +1,21 @@
+class Solution:
+	def interpret(self, command: str) -> str:
+		out = ""
+		i = 0
+		while i < len(command):
+			if command[i] == "G":
+				out += "G"
+				i += 1
+			elif command[i : i + 1] == "()":
+				out += "o"
+				i += 2
+			elif command[i : i + 4] == "(al)":
+				out += "al"
+				i += 4
+		return out
+
+
+ss = Solution()
+command = "G()()()()(al)"
+
+print(ss.interpret(command))
