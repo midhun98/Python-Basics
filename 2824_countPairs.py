c0eadb6031bae1c0bcb7421b2155e9f27ef3e073
Midhun <midhunskani@gmail.com>
2025-11-27 21:56:39 +0530

    create 2824_countPairs.py

---
+++ 2824_countPairs.py
@@ -0,0 +1,15 @@
+class Solution:
+	def countPairs(self, nums: list[int], target: int) -> int:
+		count = 0
+		for i in range(len(nums)):
+			for j in range(i + 1, len(nums)):
+				if nums[i] + nums[j] < target:
+					count += 1
+		return count
+
+
+ss = Solution()
+
+nums = [-6, 2, 5, -2, -7, -1, 3]
+target = -2
+print(ss.countPairs(nums, target))
